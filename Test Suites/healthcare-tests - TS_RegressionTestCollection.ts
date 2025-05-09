b18a071c5d9f9b54dda398b68488f3b0951fc450
lieu.huynh+core@katalon.com <katalon_g3_dummy_username>
2025-05-09 09:44:42 +0000

    Delete "DatabaseIntegritySuite" test suite

--- Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
+++ Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
@@ -6,16 +6,5 @@
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>1</maxConcurrentInstances>
-   <testSuiteRunConfigurations>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/DatabaseIntegritySuite</testSuiteEntity>
-      </TestSuiteRunConfiguration>
-   </testSuiteRunConfigurations>
+   <testSuiteRunConfigurations></testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
