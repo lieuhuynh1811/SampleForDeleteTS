4ac12e00d81ad973ac9afc436be6887b68d2a41d
lieu.huynh+core@katalon.com <katalon_g3_dummy_username>
2025-05-09 09:38:22 +0000

    Delete "EdgeCaseValidationSuite" test suite

--- Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
+++ Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
@@ -17,15 +17,5 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/DatabaseIntegritySuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/EdgeCaseValidationSuite</testSuiteEntity>
-      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
